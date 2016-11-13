b660715aed1f4c611b69b0d8542cc5a7b6dbd869
Renato Camara <renatopcamara@yahoo.com.br>
2016-11-13 13:55:50 -0200

    capitulo03-aula04 [v2]

--- app/app.ts
+++ app/app.ts
@@ -1,21 +1,34 @@
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {HomePage} from './pages/home/home';
 
+import {HomePage} from './pages/home/home';
+import {MenuTestePage} from './pages/menu-teste/menu-teste';
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
 export class MyApp {
-  rootPage: any = HomePage;
+
+    pages:  Array<{component: any, title: string, icon: string}>;
+    rootPage: any = HomePage;
 
   constructor(platform: Platform) {
+
+  this.pages = [
+    {component: HomePage, title:  'Home', icon: 'home'},
+    {component: MenuTestePage, title:  'Menu Teste', icon: 'menu'}
+  ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  openPage(page:  any) : void {
+    this.rootPage = page.component;
   }
 }
 

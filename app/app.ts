e04db720e685ca39352a19b5a57624a4e8b6aa43
Renato Camara <renatopcamara@yahoo.com.br>
2016-11-15 20:39:26 -0200

    capirtulo03-aula05 [v2]

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Platform, ionicBootstrap, MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 
 import {HomePage} from './pages/home/home';
@@ -13,7 +13,7 @@ export class MyApp {
     pages:  Array<{component: any, title: string, icon: string}>;
     rootPage: any = HomePage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, private MenuCtrl: MenuController) {
 
   this.pages = [
     {component: HomePage, title:  'Home', icon: 'home'},
@@ -27,9 +27,22 @@ export class MyApp {
     });
   }
 
-  openPage(page:  any) : void {
+  openPage(page:  any, menuside:  string) : void {
     this.rootPage = page.component;
+    this.MenuCtrl.close(menuside);
   }
+
+MenuOpened():void{
+  console.log('Abriu');
+
 }
 
-ionicBootstrap(MyApp);
+}
+
+ionicBootstrap(MyApp, [], {
+    menuType: 'push',
+    platforms: {
+      ios: {
+        menuType: 'overlay',
+      }
+    }});
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,18 @@
 import {Component} from "@angular/core";
 import {NavController} from 'ionic-angular';
 
+import {MenuTestePage} from '../menu-teste/menu-teste';
+
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navController: NavController) {
+  constructor(public navCtrl: NavController) {
 
   }
+
+  pushPage() : void{
+    this.navCtrl.push(MenuTestePage)  ;
+  }
+
 }
